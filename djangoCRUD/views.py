c82780d6e5ca710045f52d0997af44f90535d31f
Luis <lguerreroj3@unemi.edu.ec.com>
2026-06-17 22:09:49 -0500

    Agrega validaciones al formulario

--- djangoCRUD/views.py
+++ djangoCRUD/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Registros
+import re
 
 
 # Create your views here.
@@ -14,12 +15,56 @@ def agregar(request):
     correo = request.POST['correo']
     materia = request.POST['materia']
 
-    registro = Registros.objects.create(
-        nombre = nombre,
-        apellidos =apellidos,
-        correo =correo,
-        materia = materia
+    # Validar nombre
+   
+    if not nombre.replace(" ", "").isalpha():
+        registros = Registros.objects.all()
+        return render(request, 'index.html', {
+            'error': 'El nombre solo puede contener letras.',
+            'Registros': registros
+        })
+
+    # Validar apellidos
+    if not apellidos.replace(" ", "").isalpha():
+        registros = Registros.objects.all()
+        return render(request, 'index.html', {
+            'error': 'Los apellidos solo pueden contener letras.',
+            'Registros': registros
+        })
+
+    # Validar correo
+    patron_correo = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    if not re.match(patron_correo, correo):
+        registros = Registros.objects.all()
+        return render(request, 'index.html', {
+            'error': 'Ingrese un correo válido.',
+            'Registros': registros
+        })
+
+    # Validar materia
+    if not materia.replace(" ", "").isalpha():
+        registros = Registros.objects.all()
+        return render(request, 'index.html', {
+            'error': 'La materia solo puede contener letras.',
+            'Registros': registros
+        })
+
+    Registros.objects.create(
+        nombre=nombre,
+        apellidos=apellidos,
+        correo=correo,
+        materia=materia
     )
+
+    return redirect('/')
+
+    Registros.objects.create(
+        nombre=nombre,
+        apellidos=apellidos,
+        correo=correo,
+        materia=materia
+    )
+
     return redirect('/')
 
 def borrar(request, id):
